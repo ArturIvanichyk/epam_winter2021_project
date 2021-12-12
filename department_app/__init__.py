86b0bd90fd958be134f4d6dbef460286cbf377da
ArturIvanichyk <95048374+ArturIvanichyk@users.noreply.github.com>
2021-12-12 21:11:49 +0200

    Add files via upload

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -16,23 +16,48 @@ Subpackages:
 - `views`: contains modules with web controllers/views
 - `tests`: contains modules with unit tests
 """
+import os
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+	'''
+	Create flask application
+	'''
+	app = Flask(__name__)
+	app.config.from_object(Config)
+
+	db.init_app(app)
+
+	Migrate(app, db, directory=Config.MIGRATION_DIR)
+
+	from department_app.views.homepage_view import home_page
+	from department_app.views.department_view import departments_page
+	from department_app.views.employee_view import employees_page
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dep_app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+	# db.create_all()
+	app.register_blueprint(home_page)
+	app.register_blueprint(employees_page)
+	app.register_blueprint(departments_page)
 
 
-from department_app.views.homepage_view import home_page
-from department_app.views.department_view import departments_page
-from department_app.views.employee_view import employees_page
+	from department_app.rest import department_api, employee_api
+
+	api = Api(app)
+
+	api.add_resource(department_api.DepartmentListApi, '/api/departments')
+	api.add_resource(department_api.DepartmentApi, '/api/departments/<id>')
+
+	api.add_resource(employee_api.EmployeeListApi, '/api/employees')
+	api.add_resource(employee_api.EmployeeApi, '/api/employees/<id>')
 
 
-db.create_all()
-app.register_blueprint(home_page)
-app.register_blueprint(employees_page)
-app.register_blueprint(departments_page)
+	return app
