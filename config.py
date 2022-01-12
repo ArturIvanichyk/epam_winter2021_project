dca8648bc84fb4aefee278ff893253c1ca93507e
ArturIvanichyk <95048374+ArturIvanichyk@users.noreply.github.com>
2022-01-12 14:35:35 +0200

    Add testing config

--- config.py
+++ config.py
@@ -5,9 +5,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     """
     Base configuration
+
     """
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'department_app/database/dep_app.db')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MIGRATION_DIR = os.path.join('department_app', 'migrations')
+
+
+class TestingConfig(Config):
+    """
+    Test configuration
+
+    """
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'department_app/tests/test.db')
