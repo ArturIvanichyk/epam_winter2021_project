10c944df014be4e56856177276b26053330fe1c9
ArturIvanichyk <95048374+ArturIvanichyk@users.noreply.github.com>
2022-01-12 14:39:26 +0200

    Add files via upload

--- department_app/views/homepage_view.py
+++ department_app/views/homepage_view.py
@@ -12,7 +12,7 @@ home_page = Blueprint('home_page', __name__, template_folder='templates')
 @home_page.route("/home")
 def show_home():
     '''
-    Function is used to show user the home page
+    Function is used to show to user the home page
 
     '''
     return render_template('home.html')
