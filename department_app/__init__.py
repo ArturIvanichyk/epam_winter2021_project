a00a6a55402b56a23472e326cf8159e4e0736d0a
ArturIvanichyk <95048374+ArturIvanichyk@users.noreply.github.com>
2022-01-12 14:36:26 +0200

    Add files via upload

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -25,39 +25,36 @@ from flask_restful import Api
 
 from config import Config
 
-db = SQLAlchemy()
-
-def create_app():
-	'''
-	Create flask application
-	'''
-	app = Flask(__name__)
-	app.config.from_object(Config)
-
-	db.init_app(app)
-
-	Migrate(app, db, directory=Config.MIGRATION_DIR)
-
-	from department_app.views.homepage_view import home_page
-	from department_app.views.department_view import departments_page
-	from department_app.views.employee_view import employees_page
 
 
-	# db.create_all()
-	app.register_blueprint(home_page)
-	app.register_blueprint(employees_page)
-	app.register_blueprint(departments_page)
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+db.init_app(app)
+
+Migrate(app, db, directory=Config.MIGRATION_DIR)
+
+from department_app.views.homepage_view import home_page
+from department_app.views.department_view import departments_page
+from department_app.views.employee_view import employees_page
 
 
-	from department_app.rest import department_api, employee_api
-
-	api = Api(app)
-
-	api.add_resource(department_api.DepartmentListApi, '/api/departments')
-	api.add_resource(department_api.DepartmentApi, '/api/departments/<id>')
-
-	api.add_resource(employee_api.EmployeeListApi, '/api/employees')
-	api.add_resource(employee_api.EmployeeApi, '/api/employees/<id>')
+	
+app.register_blueprint(home_page)
+app.register_blueprint(employees_page)
+app.register_blueprint(departments_page)
 
 
-	return app
+from department_app.rest import department_api, employee_api
+
+api = Api(app)
+
+api.add_resource(department_api.DepartmentListApi, '/api/departments')
+api.add_resource(department_api.DepartmentApi, '/api/departments/<id>')
+
+api.add_resource(employee_api.EmployeeListApi, '/api/employees')
+api.add_resource(employee_api.EmployeeApi, '/api/employees/<id>')
+
+from department_app import models
+
+db.create_all()
