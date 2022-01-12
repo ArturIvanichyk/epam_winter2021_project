2dad0b1b9e1cdd37ae299e6f6fac4731ce1a5e29
ArturIvanichyk <95048374+ArturIvanichyk@users.noreply.github.com>
2022-01-12 14:43:26 +0200

    Add files via upload

--- department_app/database/populate.py
+++ department_app/database/populate.py
@@ -18,16 +18,13 @@ def populate_database():
 
     """
     department_1 = Item_department('Research and Development', 'Google')
-    department_2 = Item_department('Purchasing', 'Amazon', 2000)
+    department_2 = Item_department('Purchasing', 'Amazon')
     department_3 = Item_department('Human Resource Management', 'Huawei')
 
-    employee_1 = Item_employee('John Doe', '1979-12-02', 2000, department_1)
-    employee_2 = Item_employee('Jane Wilson', '1983-05-14', 2100, department_2)
-    employee_3 = Item_employee('Will Hunting', '1988-03-07', 1800, department_3)
+    employee_1 = Item_employee('John Doe', '1979-12-02', 2000, 1)
+    employee_2 = Item_employee('Jane Wilson', '1983-05-14', 2100, 2)
+    employee_3 = Item_employee('Will Hunting', '1988-03-07', 1800, 3)
 
-    department_1.employees = [employee_1]
-    department_2.employees = [employee_2]
-    department_3.employees = [employee_3]
 
     db.session.add(department_1)
     db.session.add(department_2)
