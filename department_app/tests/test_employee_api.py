455242f2c6e427186a4c4422e6051e78ce6bdc30
ArturIvanichyk <95048374+ArturIvanichyk@users.noreply.github.com>
2022-01-14 12:01:57 +0200

    Add failure tests

--- department_app/tests/test_employee_api.py
+++ department_app/tests/test_employee_api.py
@@ -63,6 +63,37 @@ class TestEmployeeApi(BaseTestCase):
                                content_type='application/json')
         assert response.status_code == http.HTTPStatus.CREATED
 
+    def test_post_failure(self):
+        """
+        Testing the post request to /api/employees.
+        It should return the status code 400
+        """
+        client = app.test_client()
+        
+        data1 = {
+            'name': 'Test Name',
+            'birth_date': '1999-11-19',
+            'tax': 2000,
+            'department_id': 1
+        }
+        data2 = None
+        data3 = {
+            'name': '',
+            'birth_date': '',
+            'salary': 2000,
+            'department_id': 1
+        } 
+        response1 = client.post('/api/employees', data=json.dumps(data1),
+                               content_type='application/json')
+        response2 = client.post('/api/employees', data=json.dumps(data2),
+                               content_type='application/json')
+        response3 = client.post('/api/employees', data=json.dumps(data3),
+                               content_type='application/json')
+
+        assert response1.status_code == http.HTTPStatus.BAD_REQUEST
+        assert response2.status_code == http.HTTPStatus.BAD_REQUEST
+        assert response2.status_code == http.HTTPStatus.BAD_REQUEST
+
     def test_put(self):
         """
         Testing the put request to /api/employees/<id>
@@ -85,6 +116,75 @@ class TestEmployeeApi(BaseTestCase):
         response = client.put(url, data=json.dumps(data),
                               content_type='application/json')
         assert response.status_code == http.HTTPStatus.OK
+
+    def test_put_failure(self):
+        """
+        Testing the put request to /api/employees/<id>
+        It should return the status code 400
+        """
+        employee = Item_employee(name='Test Name1', 
+                                birth_date='1999-11-19',
+                                salary=2000, 
+                                department_id=3)
+        db.session.add(employee)
+        db.session.commit()
+        client = app.test_client()
+        url = '/api/employees/1'
+        data1 = {
+            'nickname': 'Update Test Name1',
+            'birth': '2000-12-12',
+            'tax': 2500,
+            'department_id': 1
+        }
+        data2 = None
+        response1 = client.put(url, data=json.dumps(data1),
+                              content_type='application/json')
+        response2 = client.put(url, data=json.dumps(data2),
+                              content_type='application/json')
+
+        assert response1.status_code == http.HTTPStatus.BAD_REQUEST
+        assert response2.status_code == http.HTTPStatus.BAD_REQUEST
+
+    def test_patch(self):
+        """
+        Testing the patch request to /api/employees/<id>
+        It should return the status code 200
+        """
+        employee = Item_employee(name='Test Name1', 
+                                birth_date='1999-11-19',
+                                salary=2000, 
+                                department_id=3)
+        db.session.add(employee)
+        db.session.commit()
+        client = app.test_client()
+        url = '/api/employees/1'
+        data = {
+            'name': 'Update Test Name1'
+        }
+        response = client.patch(url, data=json.dumps(data),
+                              content_type='application/json')
+        assert response.status_code == http.HTTPStatus.OK
+
+    def test_patch_failure(self):
+        """
+        Testing the patch request to /api/employees/<id>
+        It should return the status code 400
+        """
+        employee = Item_employee(name='Test Name1', 
+                                birth_date='1999-11-19',
+                                salary=2000, 
+                                department_id=3)
+        db.session.add(employee)
+        db.session.commit()
+        client = app.test_client()
+        url = '/api/employees/1'
+        data = {
+            'some name': 'Update Test Name1'
+        }
+        response = client.patch(url, data=json.dumps(data),
+                              content_type='application/json')
+
+        assert response.status_code == http.HTTPStatus.BAD_REQUEST     
 
     def test_delete(self):
         """
