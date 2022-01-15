627631091b3959d44088b84d24ef4a4316b54210
Artur <arturfole222@gmail.com>
2022-01-15 11:46:47 +0200

    Update files

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -16,7 +16,9 @@ Subpackages:
 - `views`: contains modules with web controllers/views
 - `tests`: contains modules with unit tests
 """
+import logging.config
 import os
+import sys
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -26,20 +28,42 @@ from flask_restful import Api
 from config import Config
 
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# logging
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+
+file_handler = logging.FileHandler(filename='app.log', mode='w')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+console_handler.setLevel(logging.DEBUG)
+
+logger = app.logger
+logger.handlers.clear()
+app.logger.addHandler(file_handler)
+app.logger.addHandler(console_handler)
+app.logger.setLevel(logging.DEBUG)
+
+werkzeug_logger = logging.getLogger('werkzeug')
+werkzeug_logger.handlers.clear()
+werkzeug_logger.addHandler(file_handler)
+werkzeug_logger.addHandler(console_handler)
+werkzeug_logger.setLevel(logging.DEBUG)
+
+
 db = SQLAlchemy(app)
 db.init_app(app)
 
-Migrate(app, db, directory=Config.MIGRATION_DIR)
+migrate = Migrate(app, db, directory=Config.MIGRATION_DIR)
 
 from department_app.views.homepage_view import home_page
 from department_app.views.department_view import departments_page
 from department_app.views.employee_view import employees_page
 
-
-	
 app.register_blueprint(home_page)
 app.register_blueprint(employees_page)
 app.register_blueprint(departments_page)
@@ -56,5 +80,3 @@ api.add_resource(employee_api.EmployeeListApi, '/api/employees')
 api.add_resource(employee_api.EmployeeApi, '/api/employees/<id>')
 
 from department_app import models
-
-db.create_all()
